9e7e979aa5c95760b435754f7178fe94a27cdfb5
Seukwon Kang <kasworld@gmail.com>
2020-09-25 09:57:34 +0900

    update build

--- build.ps1
+++ build.ps1
@@ -7,7 +7,7 @@ rm -ErrorAction SilentlyContinue wasmclockcal.wasm
 $env:GOOS="js" 
 $env:GOARCH="wasm" 
 Write-Output "go build -o clientdata/wasmclockcal.wasm wasmclockcal.go"
-go build -o clientdata/wasmclockcal.wasm wasmclockcal.go
+go build -o wasmclockcal.wasm wasmclockcal.go
 $env:GOOS=""
 $env:GOARCH=""
 
