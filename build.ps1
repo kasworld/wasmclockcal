e5e39b603226d22fb35bb84691d3183dd29a8f65
Seukwon Kang <kasworld@gmail.com>
2020-12-27 15:55:02 +0900

    add cross compole linux

--- build.ps1
+++ build.ps1
@@ -1,12 +1,20 @@
 #!/usr/bin/env bash
 
+
+echo "build dir2http.exe"
 go build dir2http.go
+
+echo "build dir2http"
+$env:GOOS="linux" 
+go build dir2http.go
+$env:GOOS=""
+
 
 rm -ErrorAction SilentlyContinue wasmclockcal.wasm
 
 $env:GOOS="js" 
 $env:GOARCH="wasm" 
-Write-Output "go build -o clientdata/wasmclockcal.wasm wasmclockcal.go"
+echo "go build -o clientdata/wasmclockcal.wasm wasmclockcal.go"
 go build -o wasmclockcal.wasm wasmclockcal.go
 $env:GOOS=""
 $env:GOARCH=""
