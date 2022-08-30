bf1ef7e8f5a53d75f6c71d64798bbbbb982b42b0
Seukwon Kang <kasworld@gmail.com>
2022-08-30 09:33:17 +0900

    del linux build

--- build.ps1
+++ build.ps1
@@ -4,19 +4,19 @@
 echo "build dir2http.exe"
 go build dir2http.go
 
-echo "build dir2http"
-$env:GOOS="linux" 
-go build dir2http.go
-$env:GOOS=""
+# echo "build dir2http"
+# $env:GOOS="linux" 
+# go build dir2http.go
+# $env:GOOS=""
 
 
 rm -ErrorAction SilentlyContinue wasmclockcal.wasm
 
-$env:GOOS="js" 
-$env:GOARCH="wasm" 
+$env:GOOS = "js" 
+$env:GOARCH = "wasm" 
 echo "go build -o clientdata/wasmclockcal.wasm wasmclockcal.go"
 go build -o wasmclockcal.wasm wasmclockcal.go
-$env:GOOS=""
-$env:GOARCH=""
+$env:GOOS = ""
+$env:GOARCH = ""
 
 
